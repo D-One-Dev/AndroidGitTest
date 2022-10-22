d2c7de04392ac9c0eedfacb950da25bbb2de8dfd
misharuk05 <misharuk05@mail.ru>
2022-10-22 17:31:03 +0300

    New commit on new branch

--- app/src/main/java/com/d_one/gittest/MainActivity.java
+++ app/src/main/java/com/d_one/gittest/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Goodbye world");
+        System.out.println("New edit");
 
 
     }
